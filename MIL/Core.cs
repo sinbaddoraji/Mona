7f01aa700e68c38e361acc160cad570f8afdc2b6
Osinachi Nwagboso Samuel <sinbaddoraji@gmail.com>
2021-02-10 21:17:38 +0000

    Add project files.

---
+++ MIL/Core.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIL
+{
+    public class Core
+    {
+        #region Values
+        public dynamic Add(params object[] parameters)
+        {
+            dynamic output = null;
+
+            return output;
+        }
+        public dynamic SUB(params object[] parameters)
+        {
+            return null;
+        }
+        public dynamic MUL(params object[] parameters)
+        {
+            return null;
+        }
+        public dynamic DIV(params object[] parameters)
+        {
+            return null;
+        }
+        public dynamic LESS(object value1, object value2)
+        {
+            return null;
+        }
+        public dynamic GRT(object value1, object value2)
+        {
+            return null;
+        }
+        public dynamic EQU(object value1, object value2)
+        {
+            return null;
+        }
+        public dynamic LEQU(object value1, object value2)
+        {
+            return null;
+        }
+        public dynamic GEQU(object value1, object value2)
+        {
+            return null;
+        }
+        public dynamic COMP(object value1, object value2)
+        {
+            return null;
+        }
+        #endregion
+    }
+}
